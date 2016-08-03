0e26c7bb9a18fef72e0ab131e99e8e82f3e7e84c
bostonhulmes <1524673422@qq.com>
2016-08-03 12:16:35 +0800

    添加button

--- app/src/main/java/whretoplay/qf/com/nice/activity/MainActivity.java
+++ app/src/main/java/whretoplay/qf/com/nice/activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Toast;
@@ -37,6 +38,8 @@ public class MainActivity extends AppCompatActivity implements ViewPager.OnPageC
     RadioButton radioButton_me;
     private boolean isFirst = true;
     private long t1;
+
+    Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
