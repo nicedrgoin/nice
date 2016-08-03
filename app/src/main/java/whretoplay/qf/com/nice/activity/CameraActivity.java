46ce83e1b1083dfabd554a58544bd6ebaee2cba0
malon <getmalon@gmail.com>
2016-08-03 19:19:08 +0800

    hahahaha

---
+++ app/src/main/java/whretoplay/qf/com/nice/activity/CameraActivity.java
@@ -0,0 +1,15 @@
+package whretoplay.qf.com.nice.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import whretoplay.qf.com.nice.R;
+
+public class CameraActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+}
